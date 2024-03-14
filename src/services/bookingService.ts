78ce39a53a2770c3655a61d33de4f8f1389c6f78
Khubaib Ul Haque <khubaib_12@hotmail.com>
2024-03-14 02:17:16 +0000

    - reverting back

--- src/services/bookingService.ts
+++ src/services/bookingService.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 class BookingService {
      getBookings = async (token: string | null) => {
         try {
-          const response = await axios.get('https://malvern-api-production.up.railway.app/api/admin-bookings', {
+          const response = await axios.get('http://127.0.0.1:8000/api/admin-bookings', {
             headers: {
               Authorization: 'Bearer ' + token,
               'Content-Type': 'application/json',
@@ -18,7 +18,7 @@ class BookingService {
       // New method to create a booking
   createBooking = async (userBooking: any, token: string | null) => {
     try {
-      const response = await axios.post('https://malvern-api-production.up.railway.app/api/admin-bookings', userBooking, {
+      const response = await axios.post('http://127.0.0.1:8000/api/admin-bookings', userBooking, {
         headers: {
           Authorization: 'Bearer ' + token,
           'Content-Type': 'application/json',
