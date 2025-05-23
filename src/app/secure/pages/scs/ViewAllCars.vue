5f0f95f624c72907c359a45067c8129644952ace
Khubaib Ul Haque <khubaib_12@hotmail.com>
2025-05-23 01:14:23 +0100

    - spacing changes

--- src/app/secure/pages/scs/ViewAllCars.vue
+++ src/app/secure/pages/scs/ViewAllCars.vue
@@ -32,59 +32,45 @@ const goToCarDetail = (event: any) => {
   router.push({ name: 'car-detail', params: { id: car.id } });
 };
 
-
 onMounted(getCars);
 
 </script>
 
 <template>
-<div>
- <DataTable
-  v-model:filters="filters"
-  :value="cars"
-  filterDisplay="row"
-  sortField="id"
-  :sortOrder="-1"
-  paginator
-  :rows="10"
-  :globalFilterFields="['make', 'model', 'variant', 'price', 'mileage', 'registration']"
-  @row-click="goToCarDetail"
->
+  <div>
+    <DataTable v-model:filters="filters" :value="cars" filterDisplay="row" sortField="id" :sortOrder="-1" paginator
+      :rows="10" :globalFilterFields="['make', 'model', 'variant', 'price', 'mileage', 'registration']"
+      @row-click="goToCarDetail">
 
-  <template #header>
-    <div class="flex justify-content-between">
-      <PrimeButton type="button" icon="pi pi-filter-slash" label="Clear" outlined @click="clearFilter" />
-      <IconField iconPosition="left">
-        <InputIcon><i class="pi pi-search" /></InputIcon>
-        <InputText v-model="filters['global'].value" placeholder="Search Cars" />
-      </IconField>
-    </div>
-  </template>
+      <template #header>
+        <div class="flex justify-content-between">
+          <PrimeButton type="button" icon="pi pi-filter-slash" label="Clear" outlined @click="clearFilter" />
+          <IconField iconPosition="left">
+            <InputIcon><i class="pi pi-search" /></InputIcon>
+            <InputText v-model="filters['global'].value" placeholder="Search Cars" />
+          </IconField>
+        </div>
+      </template>
 
-  <PrimeColumn field="registration" header="Registration" sortable />
-  <PrimeColumn field="make" header="Make" sortable />
-  <PrimeColumn field="model" header="Model" sortable />
-  <PrimeColumn field="variant" header="Variant" sortable />
-  <PrimeColumn field="price" header="Price (£)" sortable>
-    <template #body="{ data }">
-      £{{ data.price ?? 'N/A' }}
-    </template>
-  </PrimeColumn>
-  <PrimeColumn field="mileage" header="Mileage" sortable>
-    <template #body="{ data }">
-      {{ data.mileage ?? 'N/A' }} miles
-    </template>
-  </PrimeColumn>
-</DataTable>
-
-</div>
-
-
+      <PrimeColumn field="registration" header="Registration" sortable />
+      <PrimeColumn field="make" header="Make" sortable />
+      <PrimeColumn field="model" header="Model" sortable />
+      <PrimeColumn field="variant" header="Variant" sortable />
+      <PrimeColumn field="price" header="Price (£)" sortable>
+        <template #body="{ data }">
+          £{{ data.price ?? 'N/A' }}
+        </template>
+      </PrimeColumn>
+      <PrimeColumn field="mileage" header="Mileage" sortable>
+        <template #body="{ data }">
+          {{ data.mileage ?? 'N/A' }} miles
+        </template>
+      </PrimeColumn>
+    </DataTable>
+  </div>
 </template>
 <style>
-
 :deep(.p-datatable-tbody > tr) {
   cursor: pointer;
 }
-
 </style>
