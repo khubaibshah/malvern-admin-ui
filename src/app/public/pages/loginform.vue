98c65e55e8f6a3a5f4128b0671aad53fe1ff155e
Khubaib Ul Haque <khubaib_12@hotmail.com>
2025-05-30 23:46:06 +0100

    maybe update

--- src/app/public/pages/loginform.vue
+++ src/app/public/pages/loginform.vue
@@ -145,7 +145,7 @@ const submitForm = async () => {
 
   try {
     await UserService.getCookie()
-    const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/login`, userDetails, {
+    const response = await axios.post(`https://malvern-api-production.up.railway.app/api/login`, userDetails, {
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
